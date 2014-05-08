7c5cce781ad81099f2afc097d2a7f58efb93d007
Francisco Esparza <francisco@Moon-OS.(none)>
2014-05-07 23:04:22 -0500

    Primeros metodos agregados

--- js/app.js
+++ js/app.js
@@ -1,55 +1,95 @@
-var BOSH_SERVICE = 'http://192.168.1.228:5280/xmpp-httpbind'
-var connection = null;
+/*
+    This program is distributed under the terms of the GPL license.
+    Please see the LICENSE file for details.
 
-function log(msg) 
-{
-    $('#log').append('<div></div>').append(document.createTextNode(msg));
+    Copyright 2014 Francisco Esparza
+*/
+/*
+ES:
+Este archivo es el encargado de realizar los procesos principales para llevar acabo las conversaciones
+El cliente funcionara mediate el protocolo XMPP y con el uso de BOSH service
+EN:
+This file is in charge of making the main processes talks
+The client worked mediate the XMPP protocol and BOSH service
+*/
+
+//Variables necesarias para la conexion
+Ximp = {
+	var BOSH_SERVICE = 'http://localhost:7070/http-bind/'
+	var connection = null;
+
+	jid_to_id: function (jid) {
+        return Strophe.getBareJidFromJid(jid)
+            .replace(/@/g, "-")
+            .replace(/\./g, "-");
+    },
+
+    on_roster: function (iq) {
+        $(iq).find('item').each(function () {
+            var jid = $(this).attr('jid');
+            var name = $(this).attr('name') || jid;
+
+            // transform jid into an id
+            var jid_id = Ximp.jid_to_id(jid);
+
+            var contact = $("<li id='" + jid_id + "'>" +
+                            "<div class='roster-contact offline'>" +
+                            "<div class='roster-name'>" +
+                            name +
+                            "</div><div class='roster-jid'>" +
+                            jid +
+                            "</div></div></li>");
+
+            Ximp.insert_contact(contact);
+        });
+
+        // set up presence handler and send initial presence
+        Ximp.connection.addHandler(Ximp.on_presence, null, "presence");
+        Ximp.connection.send($pres());
+    },
+
+     pending_subscriber: null,
+
+    on_presence: function (presence) {
+        var ptype = $(presence).attr('type');
+        var from = $(presence).attr('from');
+        var jid_id = Ximp.jid_to_id(from);
+
+        if (ptype === 'subscribe') {
+            // populate pending_subscriber, the approve-jid span, and
+            // open the dialog
+            Ximp.pending_subscriber = from;
+            $('#approve-jid').text(Strophe.getBareJidFromJid(from));
+            $('#approve_dialog').dialog('open');
+        } else if (ptype !== 'error') {
+            var contact = $('#roster-area li#' + jid_id + ' .roster-contact')
+                .removeClass("online")
+                .removeClass("away")
+                .removeClass("offline");
+            if (ptype === 'unavailable') {
+                contact.addClass("offline");
+            } else {
+                var show = $(presence).find("show").text();
+                if (show === "" || show === "chat") {
+                    contact.addClass("online");
+                } else {
+                    contact.addClass("away");
+                }
+            }
+
+            var li = contact.parent();
+            li.remove();
+            Ximp.insert_contact(li);
+        }
+
+        // reset addressing for user since their presence changed
+        var jid_id = Ximp.jid_to_id(from);
+        $('#chat-' + jid_id).data('jid', Strophe.getBareJidFromJid(from));
+
+        return true;
+    },
+	
 }
-
-function rawInput(data)
-{
-    log('RECV: ' + data);
-}
-
-function rawOutput(data)
-{
-    log('SENT: ' + data);
-}
-
-function onConnect(status)
-{
-    if (status == Strophe.Status.CONNECTING) {
-	log('Strophe is connecting.');
-    } else if (status == Strophe.Status.CONNFAIL) {
-	log('Strophe failed to connect.');
-	$('#connect').get(0).value = 'connect';
-    } else if (status == Strophe.Status.DISCONNECTING) {
-	log('Strophe is disconnecting.');
-    } else if (status == Strophe.Status.DISCONNECTED) {
-	log('Strophe is disconnected.');
-	$('#connect').get(0).value = 'connect';
-    } else if (status == Strophe.Status.CONNECTED) {
-	log('Strophe is connected.');
-	connection.disconnect();
-    }
-}
-
 $(document).ready(function () {
-    connection = new Strophe.Connection(BOSH_SERVICE);
-    connection.rawInput = rawInput;
-    connection.rawOutput = rawOutput;
 
-    $('#connect').bind('click', function () {
-	var button = $('#connect').get(0);
-	if (button.value == 'connect') {
-	    button.value = 'disconnect';
-
-	    connection.connect($('#jid').get(0).value,
-			       $('#pass').get(0).value,
-			       onConnect);
-	} else {
-	    button.value = 'connect';
-	    connection.disconnect();
-	}
-    });
 });
